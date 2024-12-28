117d9692705940eb3a64c0c77602137438450a29
Cao Xuan Long <38092914+LongCX@users.noreply.github.com>
2024-12-28 11:40:53 +0700

    Revert

--- app/src/main/java/com/vultisig/wallet/ui/models/deposit/DepositFormViewModel.kt
+++ app/src/main/java/com/vultisig/wallet/ui/models/deposit/DepositFormViewModel.kt
@@ -38,7 +38,6 @@ import java.math.BigDecimal
 import java.math.BigInteger
 import java.util.UUID
 import javax.inject.Inject
-import com.vultisig.wallet.data.models.CoinsTradeAsset
 
 internal enum class DepositOption {
     Bond,
@@ -680,7 +679,10 @@ internal class DepositFormViewModel @Inject constructor(
             )
         }
 
-        val tokenAmountInt = tokenAmount.toBigInteger()
+        val tokenAmountInt =
+            tokenAmount
+                .movePointRight(selectedToken.decimal)
+                .toBigInteger()
 
         val specific = blockChainSpecificRepository
             .getSpecific(
@@ -697,14 +699,14 @@ internal class DepositFormViewModel @Inject constructor(
             id = UUID.randomUUID().toString(),
             vaultId = vaultId,
 
-            srcToken = CoinsTradeAsset.SupportedCoins.first { it.contractAddress == "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48" },
+            srcToken = selectedToken,
             srcAddress = srcAddress,
             dstAddress = "",
 
             memo = memo.toString(),
             srcTokenValue = TokenValue(
                 value = tokenAmountInt,
-                token = CoinsTradeAsset.SupportedCoins.first { it.contractAddress == "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48" },
+                token = selectedToken,
             ),
             estimatedFees = gasFee,
             blockChainSpecific = specific.blockChainSpecific,
