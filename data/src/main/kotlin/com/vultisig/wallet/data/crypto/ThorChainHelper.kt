2e86763418ae8b10dfb63e0ea58461cc5b4bdb38
Cao Xuan Long <38092914+LongCX@users.noreply.github.com>
2025-01-03 22:18:41 +0700

    test trade asset usdc

--- data/src/main/kotlin/com/vultisig/wallet/data/crypto/ThorChainHelper.kt
+++ data/src/main/kotlin/com/vultisig/wallet/data/crypto/ThorChainHelper.kt
@@ -104,7 +104,7 @@ class ThorChainHelper(
             val coin = Cosmos.THORChainCoin.newBuilder()
                 .setAsset(
                     Cosmos.THORChainAsset.newBuilder()
-                        .setChain(chainName)
+                        .setChain("ETH")
                         .setSymbol("USDC-0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48")
                         .setTicker("USDC")
                         .setTrade(true)
